9967d7330566ee395620933f96b64345c6a0a1b4
antonyfelvyns111-afk <antony.felvyn.s111@kalvium.community>
2026-01-10 11:49:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import LoginModal from "./pages/LoginModal";
+import Cartcontxt from "./pages/Cartcontxt";
 
 function App() {
   const location = useLocation();
@@ -12,6 +13,8 @@ function App() {
       <Routes location={location}>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/cartcontext" element={<Cartcontxt />} />
+        
       </Routes>
 
       {/* Login modal on top */}
